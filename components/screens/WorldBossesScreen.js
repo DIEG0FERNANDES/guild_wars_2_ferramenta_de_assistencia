84cde8558a557541f14c9985944effa993ba251a
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-06-26 19:34:04 -0400

    update dependencias e components

--- components/screens/WorldBossesScreen.js
+++ components/screens/WorldBossesScreen.js
@@ -1,8 +1,10 @@
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { useState, useEffect } from 'react-native';
+import { useState, useEffect } from 'react';
 
 const WorldBossesScreen = () => {
   const [bosses, setBosses] = useState([]);
+
   useEffect(() => {
     fetch('https://api.guildwars2.com/v2/worldbosses')
       .then(response => response.json())
@@ -13,19 +15,25 @@ const WorldBossesScreen = () => {
         console.error(error);
       });
   }, []);
+
   if (bosses.length === 0) {
     return <Text>Carregando...</Text>;
   }
+
+  const goToHomeScreen = () => {
+    // Define your navigation logic to go back to the Home screen here
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>World Bosses Screen</Text>
-      {bosses.map((boss, index) => (
-          <View key={index.id} style={styles.bossContainer}>
-            <Text style={styles.bossName}>Nome do World Boss: {boss.name}</Text>
-            <Text style={styles.bossLocation}>{boss.location}</Text>
-            <Text>Nível Mínimo: {boss.level}</Text>
-            <Text>Tempo de Respawn: {boss.spawn_interval} segundos</Text>
-          </View>
+      {bosses.map((boss) => (
+        <View key={boss.id} style={styles.bossContainer}>
+          <Text style={styles.bossName}>Nome do World Boss: {boss.name}</Text>
+          <Text style={styles.bossLocation}>{boss.location}</Text>
+          <Text>Nível Mínimo: {boss.level}</Text>
+          <Text>Tempo de Respawn: {boss.spawn_interval} segundos</Text>
+        </View>
       ))}
       <Button title="Back to Home" onPress={goToHomeScreen} />
     </View>
