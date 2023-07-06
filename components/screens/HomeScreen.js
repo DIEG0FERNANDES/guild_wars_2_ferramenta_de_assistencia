3ecf6f08a14b515363b5aa68688936584c912891
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-07-06 09:29:23 -0400

    Update Home Screen

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -9,24 +9,28 @@ const HomeScreen = ({ navigation }) => {
         style={HomeScreenStyles.buttonContainer}
         onPress={() => navigation.navigate('World Bosses')}
       >
+        <View style={HomeScreenStyles.buttonImage} />
         <Text style={HomeScreenStyles.buttonText}>World Bosses</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={HomeScreenStyles.buttonContainer}
         onPress={() => navigation.navigate('Meta Events')}
       >
+        <View style={HomeScreenStyles.buttonImage} />
         <Text style={HomeScreenStyles.buttonText}>Meta Events</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={HomeScreenStyles.buttonContainer}
         onPress={() => navigation.navigate('Raids')}
       >
+        <View style={HomeScreenStyles.buttonImage} />
         <Text style={HomeScreenStyles.buttonText}>Raids</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={HomeScreenStyles.buttonContainer}
         onPress={() => navigation.navigate('Dungeons')}
       >
+        <View style={HomeScreenStyles.buttonImage} />
         <Text style={HomeScreenStyles.buttonText}>Dungeons</Text>
       </TouchableOpacity>
     </View>
--- components/styles/HomeScreenStyle.js
+++ components/styles/HomeScreenStyle.js
@@ -1,26 +1,34 @@
 import { StyleSheet } from "react-native";
 
 const HomeScreenStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#002727',
-    },
-    buttonContainer: {
-        width: 380,
-        marginBottom: 8,
-        marginTop: 10,
-        backgroundColor: '#00E6E6',
-        padding: 10,
-        borderRadius: 10,
-    },
-    buttonText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#06513E',
-        textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#002727',
+  },
+  buttonContainer: {
+    width: 380,
+    marginBottom: 8,
+    marginTop: 10,
+    backgroundColor: '#00E6E6',
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonImage: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#06513E',
+    marginRight: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#06513E',
+    textAlign: 'center',
+  },
 });
 
 export default HomeScreenStyles;
